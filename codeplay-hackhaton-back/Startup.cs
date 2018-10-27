6f5e4fa25512893e8d4323afc9ecd48fc527129e
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-10-27 12:18:56 +0200

    pictures status 2

--- codeplay-hackhaton-back/Startup.cs
+++ codeplay-hackhaton-back/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using codeplayhackhatonback;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,23 +28,20 @@ namespace codeplay_hackhaton_back
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddEntityFrameworkSqlite()
+            .AddDbContext<RachunekContext>(
+                options => { options.UseSqlite($"Data Source=rachunki.db"); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+           
             app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
         }
     }
 }
