2a0f654936aec3b8f47d24716641a43fdaa9b159
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-10-27 11:47:09 +0200

    pictures status

--- codeplay-hackhaton-back/Controllers/PicturesController.cs
+++ codeplay-hackhaton-back/Controllers/PicturesController.cs
@@ -18,11 +18,52 @@ namespace codeplayhackhatonback.Controllers
             return System.IO.File.ReadAllText("image.txt");
         }
 
+        [HttpGet("status")]
+        public string GetStatus(){
+            var currentStatus = "end";
+
+            try{
+                currentStatus = System.IO.File.ReadAllText("status.txt");
+            }
+            catch{
+                return "";
+            }
+
+            if(currentStatus == "start"){
+                currentStatus = "0";
+            }
+            if (currentStatus == "end")
+            {
+                return "";
+            }
+
+            var status = int.Parse(currentStatus);
+            var newStatus = status + 1;
+            if(newStatus > 4){
+                System.IO.File.WriteAllText("status.txt", "end");
+                return "";
+            }
+            System.IO.File.WriteAllText("status.txt", newStatus.ToString());
+
+            switch(status){
+                case 0:
+                    return "Otrzymano nowy rachunek ...";
+                case 1:
+                    return "Przetwarzanie rachunku ...";
+                case 2:
+                    return "Analiza zakupów ...";
+                case 3:
+                    return "Analiza gotowa";
+            }
+            return "Przetwarzanie ...";
+        }
+
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
             System.IO.File.WriteAllText("image.txt", value);
+            System.IO.File.WriteAllText("status.txt", "start");
         }
     }
 }
