dc6374629cf75b799fcfc962d42ab39797bb100a
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-10-27 10:41:27 +0200

    pictures

---
+++ codeplay-hackhaton-back/Controllers/PicturesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace codeplayhackhatonback.Controllers
+{
+    [Route("api/pictures")]
+    public class PicturesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet]
+        public string Get()
+        {
+            return System.IO.File.ReadAllText("image.txt");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            System.IO.File.WriteAllText("image.txt", value);
+        }
+    }
+}
