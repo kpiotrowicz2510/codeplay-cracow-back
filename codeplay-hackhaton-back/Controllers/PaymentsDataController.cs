864a514c09ebbf8fe08a399bfa81e5885544564a
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-10-27 13:42:15 +0200

    payments

--- codeplay-hackhaton-back/Controllers/PaymentsDataController.cs
+++ codeplay-hackhaton-back/Controllers/PaymentsDataController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +13,21 @@ namespace codeplayhackhatonback.Controllers
     [Route("api/payments")]
     public class PaymentsDataController : Controller
     {
+        private static readonly HttpClient client = new HttpClient();
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsicmViLWN1c3RvbWVyLXNlcnZpY2UiXSwidXNlcl9uYW1lIjoiTUIxQk1PNVIiLCJpZGVudGl0eSI6eyJvcmdfdW5pdCI6Im91PTk0NixvPVNHQi1CQU5LLGRjPUFDUCxkYz11ZmUsZGM9Y29tIiwicGVyc29uX2lkIjoiMzI4MDcyIiwicm9sZSI6IkNVU1RPTUVSIiwiYWNjZXNzX3Byb2ZpbGVfaWQiOiIyMzA3In0sInNjb3BlIjpbIndyaXRlIiwicmVhZCJdLCJpc3MiOiJpc3N1ZXIiLCJqdGkiOiJlYmY0ZGZmYS00ZWU2LTQ4NGUtODdiMy1hYzc5MDk3ZWRmZjUiLCJjbGllbnRfaWQiOiJyZWJSZXRhaWwiLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXX0.cQRnONmhv4WR8p3tTSjXZXYo45Xt46R_OptL-2Nj2Mg");
+
+            var stringTask = client.GetStringAsync("https://team1.asseco.pl/retail-banking-swagger/api/account?customerId=328072&accessProfileId=2307&pageNumber=1&pageSize=10");
+
+            var msg = stringTask.Result;
+
+            return msg;
         }
 
         // GET api/values/5
